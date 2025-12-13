311f0a0fe744bd735ba4ade1b5ad72a2073321fd
Person Boy <170485328+ZeldaCool@users.noreply.github.com>
2025-12-13 15:02:56 -0700

    Update crawler.py

--- src/crawler.py
+++ src/crawler.py
@@ -6,15 +6,10 @@ from whoosh import index
 import sys
 import os, os.path
 
-def main():
-    print("Select format(1,2,3):")
-    format = input()
-    page_scraper()
 
 
 def page_scraper():
     global ix
-    formatted = input()
     #add child links in schema
     schema = Schema(raw_html = TEXT(stored=True), 
             text = TEXT(stored=True),)
@@ -25,18 +20,21 @@ def page_scraper():
         soup = BeautifulSoup(response.text, 'html.parser')
         raw_text = soup.get_text()
         print(raw_text)
-        raw_text = raw_text.encode("utf-8")
+        b = str(raw_text).encode("utf-8")
+        a = str(soup).encode("utf-8")
         pretty_soup = soup.prettify()
         print(pretty_soup)
-        pretty_soup = pretty_soup.encode("utf-8")
         if not os.path.exists("indexdir"):
             os.mkdir("indexdir")
         ix = index.create_in("indexdir", schema)
-        ix = index.open_dir("index")
+        ix = index.open_dir("indexdir")
         writer = ix.writer()    
-        writer.add_document(raw_html = pretty_soup, text = raw_text)
+        #fix unicode error
+        writer.add_document(raw_html = a, text = b)
         writer.commit()
 
     else:
         print("Error in getting site!")
         sys.exit()
+
+page_scraper()
